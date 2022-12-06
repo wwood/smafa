1b89dce0b57b673a9570e2cf67eec291a5e05a7b
Ben Woodcroft <benjwoodcroft@gmail.com>
2022-12-07 06:36:46 +1000

    attempt naive with serde.

--- src/main.rs
+++ src/main.rs
@@ -1,155 +1,148 @@
-extern crate smafa;
+use std::{io::BufRead, fs::File, error::Error};
+use serde::{Serialize, Deserialize};
+use std::time::{Instant};
 
-extern crate log;
-extern crate env_logger;
-use log::LevelFilter;
-use env_logger::Builder;
+#[derive(Serialize, Deserialize, Debug)]
+struct WindowSet {
+    version: u32,
+    windows: Vec<Vec<bool>>
+}
 
-extern crate clap;
-use clap::*;
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() == 1 {
+        return encode();
+    } else if args.len() == 2 {
+        if args[1] == "decode" {
+            return decode(&"windows.cbor");
+        } else if args[1] == "query" {
+            return query(&"windows.cbor");
+        } else if args[1] == "encode" {
+            return encode();
+        } else {
+            panic!("Unknown argument: {}", args[1]);
+        }
+    } else {
+        return Err("Usage: ./main subcommand".into());
+    }
+}
 
-use std::env;
-
-fn main(){
-    let mut app = build_cli();
-    let matches = app.clone().get_matches();
-
-    match matches.subcommand_name() {
-        Some("query") => {
-            let m = matches.subcommand_matches("query").unwrap();
-            let db_root = m.value_of("DB").unwrap();
-            let query_fasta = m.value_of("QUERY_FASTA").unwrap();
-            let max_divergence = value_t!(m.value_of("divergence"), u32).unwrap_or(5);
-            set_log_level(m);
-            let k = value_t!(m.value_of("kmer-length"), usize).unwrap_or(5);
-            smafa::query(
-                db_root,
-                query_fasta,
-                max_divergence,
-                &mut std::io::stdout(),
-                k,
-                match m.is_present("amino-acid") {
-                    true => smafa::SequenceInputType::Translated,
-                    false => match m.is_present("translate") {
-                        true => smafa::SequenceInputType::Translate,
-                        false => smafa::SequenceInputType::DNA
+fn encode() -> Result<(), Box<dyn Error>> {
+    // Iterate over lines, creating a vector of bools by one hot encoding
+    // the input.
+    let encoded = std::io::stdin()
+        .lock()
+        .lines()
+        .map(|line| {
+            line.unwrap()
+                .chars()
+                .map(|c| 
+                    match c {
+                        'A' => [true,false,false,false,false],
+                        'C' => [false,true,false,false,false],
+                        'G' => [false,false,true,false,false],
+                        'T' => [false,false,false,true,false],
+                        _ => [false,false,false,false,true]
                     }
-                });
-        },
-        Some("makedb") => {
-            let m = matches.subcommand_matches("makedb").unwrap();
-            let db_fasta = m.value_of("DB_FASTA").unwrap();
-            let db_root = m.value_of("DB").unwrap();
-            set_log_level(m);
-            smafa::makedb(
-                db_root,
-                db_fasta,
-                match m.is_present("amino-acid") {
-                    true => smafa::SequenceInputType::Translated,
-                    false => match m.is_present("translate") {
-                        true => smafa::SequenceInputType::Translate,
-                        false => smafa::SequenceInputType::DNA
-                    }
-                });
-        },
-        Some("cluster") => {
-            let m = matches.subcommand_matches("cluster").unwrap();
-            let query_fasta = m.value_of("FASTA").unwrap();
-            let max_divergence = value_t!(m.value_of("divergence"), u32).unwrap_or(5);
-            set_log_level(m);
-            if m.is_present("fragment-method") {
-                smafa::fragment_clusterer::cluster_by_fragment(
-                    query_fasta, max_divergence as u8, &mut std::io::stdout());
-            } else {
-                let k = value_t!(m.value_of("kmer-length"), usize).unwrap_or(5);
-                smafa::cluster(
-                    query_fasta,
-                    max_divergence,
-                    &mut std::io::stdout(),
-                    match m.is_present("amino-acid") {
-                        true => smafa::SequenceInputType::Translated,
-                        false => match m.is_present("translate") {
-                            true => smafa::SequenceInputType::Translate,
-                            false => smafa::SequenceInputType::DNA
-                        }
-                    },
-                    k,
-                );
+                )
+                .flatten()
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+    let windows = WindowSet { 
+        version: 1,
+        windows: encoded };
+
+    // Encode
+    let ferris_file = File::create("windows.cbor")?;
+    serde_cbor::to_writer(ferris_file, &windows)?;
+    return Ok(());
+}
+
+fn decode(filename: &str) -> Result<(), Box<dyn Error>> {
+    // Decode
+    let ferris_file = File::open(filename)?;
+    let windows: WindowSet = serde_cbor::from_reader(ferris_file)?;
+
+    // Iterate over windows, decoding them into a string.
+    for window in windows.windows {
+        let mut s = String::new();
+        for i in 0..window.len() / 5 {
+            let slice = &window[i*5..(i+1)*5];
+            s.push(match slice {
+                [true, false, false, false, false] => 'A',
+                [false, true, false, false, false] => 'C',
+                [false, false, true, false, false] => 'G',
+                [false, false, false, true, false] => 'T',
+                _ => 'x'
+            });
+        }
+        println!("{}", s);
+    }
+    return Ok(());
+}
+
+fn query(filename: &str) -> Result<(), Box<dyn Error>> {
+    // Decode
+    let mut start = Instant::now();
+    let ferris_file = File::open(filename)?;
+    let windows: WindowSet = serde_cbor::from_reader(ferris_file)?;
+    eprintln!("Decoded in {}ms", start.elapsed().as_millis()); start = Instant::now();
+
+    // encode a line from stdin as a vector of bools
+    let query_vec = std::io::stdin()
+        .lock()
+        .lines()
+        .next()
+        .unwrap()
+        .unwrap()
+        .chars()
+        .map(|c| 
+            match c {
+                'A' => [true,false,false,false,false],
+                'C' => [false,true,false,false,false],
+                'G' => [false,false,true,false,false],
+                'T' => [false,false,false,true,false],
+                _ => [false,false,false,false,true]
             }
-        },
-        _ => {
-            app.print_help().unwrap();
-            println!();
+        )
+        .flatten()
+        .collect::<Vec<_>>();
+
+    // Get the minimum distance between the query and each window using xor.
+    let distances = windows.windows
+        .iter()
+        .map(|window| {
+            window.iter()
+                .zip(query_vec.iter())
+                .map(|(a, b)| a ^ b)
+                .filter(|x| *x)
+                .count()
+        })
+        .collect::<Vec<_>>();
+    eprintln!("Distanced in {}ms", start.elapsed().as_millis()); start = Instant::now();
+
+    // Find the minimum distance.
+    let min_distance = distances.iter().min().unwrap();
+    eprintln!("Found minimum in {}ms", start.elapsed().as_millis()); start = Instant::now();
+
+    // Print the windows with the minimum distance.
+    for (i, distance) in distances.iter().enumerate() {
+        if distance == min_distance {
+            let mut s = String::new();
+            for j in 0..windows.windows[i].len() / 5 {
+                let slice = &windows.windows[i][j*5..(j+1)*5];
+                s.push(match slice {
+                    [true, false, false, false, false] => 'A',
+                    [false, true, false, false, false] => 'C',
+                    [false, false, true, false, false] => 'G',
+                    [false, false, false, true, false] => 'T',
+                    _ => 'x'
+                });
+            }
+            println!("{} {} {}", min_distance, i, s);
         }
     }
+    eprintln!("Printed in {}ms", start.elapsed().as_millis());
+    return Ok(());
 }
-
-fn set_log_level(matches: &clap::ArgMatches) {
-    let mut log_level = LevelFilter::Info;
-    if matches.is_present("verbose") {
-        log_level = LevelFilter::Debug;
-    }
-    if matches.is_present("quiet") {
-        log_level = LevelFilter::Error;
-    }
-    let mut builder = Builder::new();
-    builder.filter_level(log_level);
-    if env::var("RUST_LOG").is_ok() {
-        builder.parse_filters(&env::var("RUST_LOG").unwrap());
-    }
-    builder.try_init().unwrap();
-}
-
-fn build_cli() -> App<'static, 'static> {
-    let makedb_args: &'static str = "<DB_FASTA>  'Subject sequences to search against'
-                       <DB>        'Output DB filename root'
-                       --amino-acid  'Sequences are amino acid [default: nucleotide]'
-
-                      -v, --verbose       'Print extra debug logging information'
-                      -q, --quiet         'Unless there is an error, do not print logging information'";
-    let query_args: &'static str = "<DB>           'Output from makedb'
-                      <QUERY_FASTA> 'Query sequences to search with'
-                      -d, --divergence=[INTEGER] 'Maximum number of mismatches in reported hits [default: 5]'
-                      -k, --kmer-length=[INTEGER]   'Length of kmer to query with [default 5]'
-
-                      -v, --verbose       'Print extra debug logging information'
-                      -q, --quiet         'Unless there is an error, do not print logging information'";
-    let cluster_args: &'static str = "<FASTA> 'Sequences to cluster'
-                      -d, --divergence=[INTEGER] 'Maximum number of mismatches in reported hits [default: 5]'
-                       --amino-acid  'Sequences are amino acid [default: nucleotide]'
-                      -k, --kmer-length=[INTEGER]   'Length of kmer to query with [default 5]'
-
-                      -v, --verbose       'Print extra debug logging information'
-                      -q, --quiet         'Unless there is an error, do not print logging information'";
-
-    return App::new("smafa")
-        .version(env!("CARGO_PKG_VERSION"))
-        .author("Ben J. Woodcroft <benjwoodcroft near gmail.com>")
-        .about("Read aligner for small pre-aligned sequences")
-        .args_from_usage("-v, --verbose       'Print extra debug logging information'
-             -q, --quiet         'Unless there is an error, do not print logging information'")
-        .subcommand(
-            SubCommand::with_name("makedb")
-                .about("Generate a searchable database")
-                .args_from_usage(&makedb_args)
-                .arg(Arg::with_name("translate")
-                     .long("translate")
-                     .help("Translate input before generating DB")
-                     .conflicts_with("amino-acid")))
-        .subcommand(
-            SubCommand::with_name("query")
-                .about("Search a database")
-                .args_from_usage(&query_args)
-                .arg(Arg::with_name("translate")
-                     .long("translate")
-                     .help("Translate input before generating DB")
-                     .conflicts_with("amino-acid")))
-        .subcommand(
-            SubCommand::with_name("cluster")
-                .about("Cluster sequences greedily, preferring sequences towards front of file")
-                .arg(Arg::with_name("fragment-method")
-                     .long("fragment-method")
-                     .help("Use the 'fragment' method for clustering"))
-                .args_from_usage(&cluster_args));
-}
-
